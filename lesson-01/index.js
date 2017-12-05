3fc3f08c65f2f5e9122976e1096638bf5b4367c4
chasingcar <253404275@qq.com>
2017-12-05 10:40:29 +0800

    second

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -5,17 +5,46 @@ const hostname = '127.0.0.1';
 const port = 1308;
 
 const now = () => new Date().toISOString().replace('T', ' ').slice(0, 19);
+const retriveProperty = (data, key) => {
+  if(key.indexOf('.') > -1){
+    const keys=key.split('.')
+    let value=data[keys[0]];
+    for(let i=1;i<keys.length;i++){
+        if(!value){
+         break
+        }
+        value=value[keys[i]]
+    }
+    return value
+  }
+  return data[key] || ''
+}
+function render(template, data) {
+
+  const param = template.match(/({{[^}]*}})/g).map(v => v.replace(/[{}]/g,''))
+  console.log(param)
+  let html = template;
+  param.forEach(v => {
+    html=html.replace(`{{${v}}}`,retriveProperty(data,v))
+  })
+  return html;
+}
 
 const server = http.createServer((req, res) => {
   console.log(`${now()} request ${req.url}`);
-  const html = fs.readFileSync('./views/index.ejs').toString();
-  const rendered = html
-    .replace('<%= title%>', 'Node & JavaScript')
-    .replace('<%= content%>', 'This is one lesson about nodejs and javascript.');
+  const data = {
+    title: 'Lesson 01',
+    content: {
+      head: 'A Simple Template',
+      paragraph: 'Good artists copy; great artists steal — Pablo Picasso',
+    },
+  }
+  const template = fs.readFileSync('./views/index.template').toString();
+  const html = render(template, data);
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(rendered);
+  res.end(html);
 });
 
 server.listen(port, hostname, () => {
